520d970883e65edba76cd1c28a60dba0a28b6338
Holger Partsch (D057760) <holger.partsch@sap.com>
2023-04-07 13:06:16 +0200

    Reformat

--- src/lib/tracking.ts
+++ src/lib/tracking.ts
@@ -1,28 +1,27 @@
-'use strict'
+"use strict";
 
-import { Octokit } from "octokit"
-import { Repository } from "./gitContext"
+import { Octokit } from "octokit";
+import { Repository } from "./gitContext";
 
 export class Tracker {
-    _octokit: Octokit;
+  _octokit: Octokit;
 
-    constructor(octokit: Octokit) {
-        this._octokit = octokit;
+  constructor(octokit: Octokit) {
+    this._octokit = octokit;
+  }
+
+  async addNewPr(repo: Repository, prIdentifier: string) {
+    const pullNumber = Number(prIdentifier);
+    if (isNaN(pullNumber)) {
+      throw new Error("PR identifier must be a number");
     }
-
-    async addNewPr(repo: Repository, prIdentifier: string) {
-        const pullNumber = Number(prIdentifier);
-        if (isNaN(pullNumber)) {
-            throw new Error("PR identifier must be a number")
-        }
-        const pr = await this._octokit.rest.pulls.get({
-            owner: repo.owner, repo: repo.name,
-            pull_number: pullNumber
-        })
-        if ('data' in pr) {
-            console.log(pr.data);
-        }
+    const pr = await this._octokit.rest.pulls.get({
+      owner: repo.owner,
+      repo: repo.name,
+      pull_number: pullNumber,
+    });
+    if ("data" in pr) {
+      console.log(pr.data);
     }
-
+  }
 }
-
--- src/run.ts
+++ src/run.ts
@@ -1,58 +1,57 @@
-'use strict'
+"use strict";
 
-import { Octokit } from "octokit"
-import * as path from 'path'
-import * as fs from 'node:fs'
-import { Command } from 'commander'
-
+import { Octokit } from "octokit";
+import * as path from "path";
+import * as fs from "node:fs";
+import { Command } from "commander";
 
 // own modules
-import { getRepository } from './lib/gitContext'
-import { Tracker } from './lib/tracking'
-
+import { getRepository } from "./lib/gitContext";
+import { Tracker } from "./lib/tracking";
 
 function readConfig() {
-    const home: string = process.env.HOME as string;
-    const configFileName = path.join(home, '.github_bot.json');
-    const json = fs.readFileSync(configFileName, { encoding: 'utf8' });
-    return JSON.parse(json);
+  const home: string = process.env.HOME as string;
+  const configFileName = path.join(home, ".github_bot.json");
+  const json = fs.readFileSync(configFileName, { encoding: "utf8" });
+  return JSON.parse(json);
 }
 
 const config = readConfig();
 
 console.log(config);
 
-const pkg = require('../package.json');
+const pkg = require("../package.json");
 const appName = Object.keys(pkg.bin)[0];
 
-const program = new Command()
+const program = new Command();
 program
-    .description(pkg.description)
-    .name(appName)
-    .option('-c, --config <path>', 'set path to configuration file')
-    .version(pkg.version);
+  .description(pkg.description)
+  .name(appName)
+  .option("-c, --config <path>", "set path to configuration file")
+  .version(pkg.version);
 
-program.command('track')
-    .description('keep track of a pull request')
-    .argument('<prIdentifier>', 'Pull request number or branch name')
-    .action(async function(prIdentifier) {
-        console.log('tracking ', prIdentifier)
-        const repo = getRepository();
-        const client = getClientForHost(repo.host)
-        const tracker = new Tracker(client);
-        await tracker.addNewPr(repo, prIdentifier)
-    });
+program
+  .command("track")
+  .description("keep track of a pull request")
+  .argument("<prIdentifier>", "Pull request number or branch name")
+  .action(async function (prIdentifier) {
+    console.log("tracking ", prIdentifier);
+    const repo = getRepository();
+    const client = getClientForHost(repo.host);
+    const tracker = new Tracker(client);
+    await tracker.addNewPr(repo, prIdentifier);
+  });
 
 program.parseAsync();
 
 function getClientForHost(host: string) {
-    const server = config.servers.find((server: any) => server.host === host);
-    if (!server) {
-        throw new Error(`Cannot find configuration for host ${host}.`)
-    }
-    if ("token" in server) {
-        return new Octokit({ auth: server.token, baseUrl: server.baseUrl })
-    } else {
-        throw new Error(`No token found in configuration for host ${host}.`)
-    }
+  const server = config.servers.find((server: any) => server.host === host);
+  if (!server) {
+    throw new Error(`Cannot find configuration for host ${host}.`);
+  }
+  if ("token" in server) {
+    return new Octokit({ auth: server.token, baseUrl: server.baseUrl });
+  } else {
+    throw new Error(`No token found in configuration for host ${host}.`);
+  }
 }
